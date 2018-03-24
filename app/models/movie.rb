112a4e98a5abaa8559e9b3aae4e09ca55f8ebb4d
Diogo Lima <srdiogolima@gmail.com>
2018-03-23 21:13:24 -0300

    - Creating review
    - Creating migration and views
    - Add association movie/review
    - Edit css
    - Add route

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, :description, :movie_length, :director, :rating, presence: true
   belongs_to :user
+  has_many :reviews
   has_attached_file :image, styles: { medium: "480x640#", thumb: "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
