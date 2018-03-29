dbd6a2dec9987f507cb2bb42bd9d772cdd45537b
Diogo Lima <srdiogolima@gmail.com>
2018-03-29 10:42:10 -0300

    - Add ransack gem
    - Add gem ransack
    - Creating form_search for ransack
    - Editing movies_controller to work with ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_ransack
+
+  private
+  
+  def set_ransack
+    @search = Movie.ransack(params[:q])
+  end
+
 end
