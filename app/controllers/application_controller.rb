76c0ca467251319321c8bd2e5a22e29786c33284
Diogo Lima <srdiogolima@gmail.com>
2018-03-29 10:42:30 -0300

    Revert "- Add ransack gem"
    This reverts commit dbd6a2dec9987f507cb2bb42bd9d772cdd45537b.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_ransack
-
-  private
-  
-  def set_ransack
-    @search = Movie.ransack(params[:q])
-  end
-
 end
