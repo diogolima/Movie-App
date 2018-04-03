808413824c425a8182a046bc8f9b58083961fb03
Diogo Lima <srdiogolima@gmail.com>
2018-04-03 10:22:57 -0300

    - Testing movie controller
    Movie controller minitest

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @movie = movies(:one)
+    @movie_test = movies(:one)
+    sign_in_for(@movie_test)
   end
 
   test "should get index" do
@@ -17,32 +20,52 @@ class MoviesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create movie" do
     assert_difference('Movie.count') do
-      post movies_url, params: { movie: { description: @movie.description, director: @movie.director, movie_length: @movie.movie_length, rating: @movie.rating, title: @movie.title } }
+      post movies_url, params: { movie: { description: @movie_test.description, director: @movie_test.director, movie_length: @movie_test.movie_length, rating: @movie_test.rating, title: @movie_test.title } }
     end
 
     assert_redirected_to movie_url(Movie.last)
   end
 
   test "should show movie" do
-    get movie_url(@movie)
+    get movie_url(@movie_test)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_movie_url(@movie)
+    get edit_movie_url(@movie_test)
     assert_response :success
   end
 
+  test "should not get edit" do
+    att_diff_user(@movie_test)
+    get edit_movie_url(@movie_test)
+    assert_redirected_to movies_url
+  end
+
   test "should update movie" do
-    patch movie_url(@movie), params: { movie: { description: @movie.description, director: @movie.director, movie_length: @movie.movie_length, rating: @movie.rating, title: @movie.title } }
-    assert_redirected_to movie_url(@movie)
+    patch movie_url(@movie_test), params: { movie: { description: @movie_test.description, director: @movie_test.director, movie_length: @movie_test.movie_length, rating: @movie_test.rating, title: @movie_test.title} }
+    assert_redirected_to movie_url(@movie_test)
+  end
+
+  test "should not update movie" do
+    att_diff_user(@movie_test)
+    patch movie_url(@movie_test), params: { movie: { description: @movie_test.description, director: @movie_test.director, movie_length: @movie_test.movie_length, rating: @movie_test.rating, title: @movie_test.title} }
+    assert_redirected_to movies_url
   end
 
   test "should destroy movie" do
     assert_difference('Movie.count', -1) do
-      delete movie_url(@movie)
+      delete movie_url(@movie_test)
     end
-
     assert_redirected_to movies_url
   end
+
+  test "should not destroy movie" do
+    att_diff_user(@movie_test)
+    assert_difference('Movie.count', 0) do
+      delete movie_url(@movie_test)
+    end
+    assert_redirected_to movies_url
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,20 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def sign_in(user)
+    post user_session_path \
+      "user[email]"    => user.email,
+      "user[encrypted_password]" => user.password
+  end
+
+  def sign_in_for(subject)
+    @user = User.create(email: "myemail@me.com", encrypted_password: 'password', id: 1, created_at: Time.now, updated_at: Time.now)
+    sign_in @user
+    subject.update_attribute(:user, @user)
+  end
+
+  def att_diff_user(subject)
+    @user2 = User.create(email: "myemail@me.com", encrypted_password: 'password', id: 2, created_at: Time.now, updated_at: Time.now)
+    subject.update_attribute(:user, @user2)
+  end
 end
